17a6dcdea864cbf36f1b8685ffba88a6a4f9220e
HarryHongg <57870719+HarryHongg@users.noreply.github.com>
2022-02-13 22:04:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-import random
-
 from Board import *
-from Minion import *
+
 from Magic import *
 
-num_my_minion = random.randrange(3, 7)
-num_enemies = random.randrange(num_my_minion, 7)
-my_minions = []
-enemies = []
-for i in range(num_my_minion):
-    my_minions.append(Minion(chr(random.randint(65, 122)), random.randint(1, 8), random.randint(1, 9)))
-for i in range(num_enemies):
-    enemies.append(Minion(chr(random.randint(65, 122)), random.randint(1, 8), random.randint(1, 9)))
 
-board = Board(my_minions, enemies)
+
+board = Board()
 
 
 
